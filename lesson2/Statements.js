e4b704783616c1fbb58715d4595c67aadc441af2
Wadym_IH <glutter@live.ru>
2018-12-20 20:40:58 +0200

    Updated String_ex2 and Statement

--- lesson2/Statements.js
+++ lesson2/Statements.js
@@ -1,25 +1,35 @@
-const instr = ['Back', 'Back', 'Back', 'Back', 'Forward', 'Left', 'Right', 'Right'];
+const instr = ['Forward','Back', 'Back', 'Back', 'Back', 'Forward', 'Left', 'Right', 'Right', 'Right', 'Right', 'Right', 'Right', 'Right' ];
 
-var x = 0
-var y = 0
-var tempArr = []
+function retutnPath(instructions) {
+	let x = 0;
+	let y = 0;
+	let path = [];
+	path.push('Начало ' + [x, y]);
+	let limit = true;
+	if (limit) {
+		instructions.forEach(function (item) {
+			if (x <= 2 && x >= -2 && y <= 2 && y >= -2) {
+				/* я не понял почему но если пишу условие "x <= 3 && x >= -3 && y <= 3 && y >= -3" то на
+        выходе захватывает и координаты с "Четверкой",
+        а если пишу "x <= 2 && x >= -2 && y <= 2 && y >= -2" то условие выполняется. */
+				if (item === 'Forward') {
+					x++;
+					path.push([x, y]);
+				} else if (item === 'Back') {
+					x--;
+					path.push([x, y]);
+				} else if (item === 'Right') {
+					y++;
+					path.push([x, y]);
+				} else if (item === 'Left') {
+					y--;
+					path.push([x, y]);
+				}
+			}
+		});
+	}
+	path.push('Конец ' + [x, y]);
+	return path;
+}
 
-tempArr.push([x,y])
-
-instr.forEach(function (item) {
-    if (item === 'Forvard') {
-      x++
-      tempArr.push([x,y])
-    } else if (item === 'Back') {
-      x--
-      tempArr.push([x,y])
-    }
-    else if (item === 'Right') {
-      y++
-      tempArr.push([x,y])
-    } else if (item === 'Left') {
-      y--
-      tempArr.push([x,y])
-    }
-  console.log(tempArr);
-})
+console.log(retutnPath(instr));
--- lesson2/String_ex2.js
+++ lesson2/String_ex2.js
@@ -8,16 +8,16 @@ let findStr4 = 'is';
 let findStr5 = 'is my test';
 
 function getSubString(str, subStr) {
-  if (str.indexOf(subStr) !== -1) {
-    let index = str.indexOf(subStr)
-    let subStrLengts = subStr.length
-    console.log(index);
+	if (str.indexOf(subStr) !== -1) {
+		let index = str.indexOf(subStr);
+		let subStrLengts = subStr.length;
+		console.log(index);
 
-    return str.slice(index, index + subStrLengts)
-  }
+		return str.slice(index, index + subStrLengts);
+	}
 }
-console.log(getSubString(testString, findStr1))
-console.log(getSubString(testString, findStr2))
-console.log(getSubString(testString, findStr3))
-console.log(getSubString(testString, findStr4))
-console.log(getSubString(testString, findStr5))
+console.log(getSubString(testString, findStr1));
+console.log(getSubString(testString, findStr2));
+console.log(getSubString(testString, findStr3));
+console.log(getSubString(testString, findStr4));
+console.log(getSubString(testString, findStr5));
