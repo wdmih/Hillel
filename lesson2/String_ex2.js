70250b78a164f528fef6baeab105d8f9608d7599
Wadym <glutter@live.ru>
2018-12-20 11:12:57 +0200

    added sting_ex2

--- lesson2/String_ex2.js
+++ lesson2/String_ex2.js
@@ -1,1 +1,23 @@
 // #2 STRING OPERATIONS
+const testString = 'this is my test string';
+
+let findStr1 = 'string';
+let findStr2 = 'this';
+let findStr3 = 'this is';
+let findStr4 = 'is';
+let findStr5 = 'is my test';
+
+function getSubString(str, subStr) {
+  if (str.indexOf(subStr) !== -1) {
+    let index = str.indexOf(subStr)
+    let subStrLengts = subStr.length
+    console.log(index);
+
+    return str.slice(index, index + subStrLengts)
+  }
+}
+console.log(getSubString(testString, findStr1))
+console.log(getSubString(testString, findStr2))
+console.log(getSubString(testString, findStr3))
+console.log(getSubString(testString, findStr4))
+console.log(getSubString(testString, findStr5))
