640fc169e9aee10c4ddb24c035a7f47ce96da7eb
Wadym_IH <glutter@live.ru>
2018-12-25 00:17:01 +0200

    fixed a condition error

--- lesson2/Statements.js
+++ lesson2/Statements.js
@@ -1,65 +1,40 @@
-const instr = ['Forward','Back', 'Back', 'Back', 'Forward', 'Left', 'Right', 'Right', 'Right', 'Right', 'Right', 'Right', 'Right' ];
+const instr = ['Forward','Back', 'Back', 'Back', 'Back', 'Left', 'Right', 'Right', 'Right', 'Right', 'Right', 'Right', 'Right' ];
 
-// Variant #1
 function returnPath(instructions) {
 	let x = 0;
 	let y = 0;
 	let path = [];
 	path.push('Начало ' + [x, y]);
-	instructions.forEach(function (item) {
-		if (x < 3 && x > -3 && y < 3 && y > -3) {
-			if (item === 'Forward') {
-				x++;
-				path.push([x, y]);
-			} else if (item === 'Back') {
-				x--;
-				path.push([x, y]);
-			} else if (item === 'Right') {
-				y++;
-				path.push([x, y]);
-			} else if (item === 'Left') {
-				y--;
-				path.push([x, y]);
+
+	for (let i = 0; i < instructions.length; i++) {
+		if (instructions[i] === 'Forward') {
+			if (x >= 3) {
+				break;
 			}
-		}
-	});
-	path.push('Конец ' + [x, y]);
-	return path;
-}
-console.log(returnPath(instr));
-
-
-
-// Variant #2
-function returnPath2(instructions) {
-	let x = 0;
-	let y = 0;
-	let path = [];
-	path.push('Начало ' + [x, y]);
-
-	function logPath(arr, item) {
-		if (arr[item] === 'Forward') {
 			x++;
 			path.push([x, y]);
-		} else if (arr[item] === 'Back') {
+		} else if (instructions[i] === 'Back') {
+			if (x <= -3) {
+				break;
+			}
 			x--;
 			path.push([x, y]);
-		} else if (arr[item] === 'Right') {
+		} else if (instructions[i] === 'Right') {
+			if (y >= 3) {
+				break;
+			}
 			y++;
 			path.push([x, y]);
-		} else if (arr[item] === 'Left') {
+		} else if (instructions[i] === 'Left') {
+			if (y <= -3) {
+				break;
+			}
 			y--;
 			path.push([x, y]);
 		}
 	}
 
-	for (let i = 0; i < instructions.length; i++) {
-		if (x < 3 && x > -3 && y < 3 && y > -3) {
-			logPath(instructions, i);
-		}
-	}
-
 	path.push('Конец ' + [x, y]);
 	return path;
 }
-console.log(returnPath2(instr));
+console.log(returnPath(instr));
