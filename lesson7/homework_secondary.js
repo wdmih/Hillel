2a7ebe25fff006805fbc9d79fc88c8652342d5ed
Wadym_IH <glutter@live.ru>
2019-01-21 20:55:08 +0200

    Create homework_secondary

---
+++ lesson7/homework_secondary.js
@@ -0,0 +1,52 @@
+const lib = (function() {
+  return {
+    clone: function arrDeepCopy(arr) {
+      let copy = [];
+      for (let i = 0; i < arr.length; i++) {
+        if (typeof arr[i] !== 'object') {
+          copy[i] = arr[i];
+        } else {
+          copy[i] = arrDeepCopy(arr[i]);
+        }
+      }
+      return copy;
+    },
+    copy: function copyArr(arr) {
+      let copiedArr = [];
+      for (let i = 0; i < arr.length; i++) {
+        copiedArr[i] = arr[i];
+      }
+      return copiedArr;
+    },
+    deleteMin: function deleteMinS(arr) {
+      let min = Math.min(...arr);
+      arr.forEach((v, k, arr) => v !== min || arr.splice(k, 1));
+      return arr;
+    },
+    sortArr: function sortArr(arr, dir) { // dir = true - low to high, false - high to low
+      function lowToHigh(a, b) {
+        if (a.length > b.length) {
+          return 1;
+        } else if (a.length < b.length) {
+          return -1;
+        } else {
+          return 0;
+        }
+      }
+      function highToLow(a, b) {
+        if (a.length < b.length) {
+          return 1;
+        } else if (a.length > b.length) {
+          return -1;
+        } else {
+          return 0;
+        }
+      }
+      if (dir) {
+        return arr.sort(lowToHigh);
+      } else {
+        return arr.sort(highToLow);
+      }
+    }
+  };
+})();
